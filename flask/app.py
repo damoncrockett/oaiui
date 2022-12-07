aee08032ddf67bdf80b42f63d78c11925f3a5814
damoncrockett <damoncrockett@hotmail.com>
2022-12-07 00:07:53 -0500

    stopwords

--- flask/app.py
+++ flask/app.py
@@ -44,7 +44,19 @@ def save_gpt():
         data = request.json
         text = data['text']
 
-    fstring = '_'.join([item[0] for item in Counter(text.split(" ")).most_common()[:16]])
+    word_counts = [item[0].strip().lower() for item in Counter(text.split(" ")).most_common()]
+    
+    stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he',
+                'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 
+                'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 
+                'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 
+                'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 
+                'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 
+                'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 
+                'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 'can', 
+                'will', 'just', 'don', 'should', 'now']
+
+    fstring = '_'.join([item for item in word_counts if item not in stopwords][:16])
     savestr = get_savestr(SAVEGPT + fstring + ".txt")
 
     try:
